f3216d134d82c61bfca448a5880445734d531e8c
Armoghan <armoghan16@gmail.com>
2024-05-24 00:36:37 +0500

    Created User Model and Table

--- app/database.py
+++ app/database.py
@@ -1,10 +1,31 @@
 import os
+from sqlalchemy import inspect
 from app import db ,app
 from utilities import *
-# from models import *
+from models import *
 
 # Get the current working directory
 cwd = os.getcwd()
+
+def create_tables_if_not_exists(db, table_names):
+    try:
+        with app.app_context():
+            inspector = inspect(db.engine)
+            for table_name in table_names:
+                if table_name not in inspector.get_table_names():
+                    # Explicitly create the table
+                    table_class = globals()[table_name.capitalize()]  # Assuming your table class names are capitalized
+                    table_class.__table__.create(db.engine)
+                    print()
+                    print(color.ColorPrinter.magenta(f"Creating {table_name.capitalize()} Table"), end=" ")
+                    dots.print_colored_dots(10 , 'magenta')
+                    print()
+                    print(color.ColorPrinter.green(f"{table_name.capitalize()} Table Created Successfully"))
+                else:
+                    print()
+                    print(color.ColorPrinter.yellow(f"{table_name.capitalize()} Table already exists"))
+    except Exception as e:
+        print(color.ColorPrinter.red(f"Error creating tables: {e}"))
     
 clear.TerminalClear.clear()
 banner.print_banner()
@@ -25,3 +46,6 @@ try:
             print(color.ColorPrinter.yellow("Database already exists"))
 except Exception as e:
     print(color.ColorPrinter.red(f"Error creating database: {e}"))
+    
+table_names = ['user']
+create_tables_if_not_exists(db, table_names)
