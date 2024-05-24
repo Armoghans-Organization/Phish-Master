cd5f42dee2d55b296987af808260a4e45b9aec0f
Armoghan <Armoghan16@gmail.com>
2024-05-24 18:32:19 +0500

    Added Login Page Template and Route

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask , render_template
 from config import Config, db
+from utilities import banner
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -9,6 +10,11 @@ app.config.from_object(Config)
 # Initialize SQLAlchemy with the Flask app
 db.init_app(app)
 
+@app.route("/")
+def index():
+  return render_template("index.html")
 
 if __name__ == '__main__':
+  #  Print Terminal Banner
+  banner.print_banner()
   app.run(port=5000,debug=True)
